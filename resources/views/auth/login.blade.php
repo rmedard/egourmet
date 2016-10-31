95625d5e9d72ae7a1c95b4fdef3ac12aad99eddd
Medard Rebero <rmedard5@gmail.com>
2016-10-31 04:06:36 +0100

    First online

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,13 +3,12 @@
 @section('content')
 <div class="container" id="login-page">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-2" style="margin-top: 20px;">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
