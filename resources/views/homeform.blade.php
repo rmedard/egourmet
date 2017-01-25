0e5a40eaf672513f69f1461fad58ea8e24ffd3db
Medard Rebero <rmedard5@gmail.com>
2017-01-25 19:24:29 +0100

    Facebook share

--- resources/views/homeform.blade.php
+++ resources/views/homeform.blade.php
@@ -64,7 +64,7 @@
                     </div>
                     {!! Form::close() !!}
 
-                    <div class="card-data grey">
+                    <div class="card-data grey lighten-2">
                         <div class="fb-share-button" data-href="http://www.egourmet.be" data-layout="button_count" data-size="large" data-mobile-iframe="true">
                             <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.egourmet.be%2F&amp;src=sdkpreparse">Share</a>
                         </div>
